c98630c34d4a81c4865120e090b24d65da043fcd
Tomaz Wang <TomazWang.tw@gmail.com>
2015-03-12 21:17:42 +0800

    adding new view (home) using template tag

--- myfirstsite/urls.py
+++ myfirstsite/urls.py
@@ -12,4 +12,5 @@ urlpatterns = patterns('',
     url(r'^hello/$', 'trips.views.hello_world'),
     url(r'^hello_html/$', 'trips.views.hello_html'),
     url(r'^hello_temp/$', 'trips.views.hello_temp'),
+    url(r'^$','trips.views.home'), # direct to home
 )
--- trips/views.py
+++ trips/views.py
@@ -31,3 +31,20 @@ def hello_html(request):
 # use a template
 def hello_temp(request):
 	return render(request,'hello_temp.html',{'current_time':datetime.now()})
+
+
+'''
+	start making my first site with not just hello world
+'''
+# import the Post obj from model
+from trips.models import Post
+
+# home page ,show all the posts
+def home(request):
+	post_list = Post.objects.all()
+	return render(request,
+		'home.html',
+		{'post_list':post_list})
+
+
+
