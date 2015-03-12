6fb9b46ecacf50a021eef4df20c02a0afeda0571
Tomaz Wang <TomazWang.tw@gmail.com>
2015-03-12 16:17:37 +0800

    basic hello world

--- myfirstsite/urls.py
+++ myfirstsite/urls.py
@@ -5,6 +5,11 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'myfirstsite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    #
+    # url(regex,view)
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^hello/$', 'trips.views.hello_world'),
+    url(r'^hello_html/$', 'trips.views.hello_html'),
+    url(r'^hello_temp/$', 'trips.views.hello_temp'),
 )
