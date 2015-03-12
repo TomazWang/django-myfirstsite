4c85b909c4fdd800ee50f46147825a33422f7322
Tomaz Wang <TomazWang.tw@gmail.com>
2015-03-12 16:38:52 +0800

    adding model

--- myfirstsite/settings.py
+++ myfirstsite/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'trips', # adding my own app
 )
 
 MIDDLEWARE_CLASSES = (
--- trips/models.py
+++ trips/models.py
@@ -1,3 +1,21 @@
+# trips/models.py
+
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+	title = models.CharField(max_length = 100)
+	content = models.TextField(blank=True)
+	photo = models.URLField(blank=True)
+	location = models.CharField(max_length = 100)
+	created_at = models.DateTimeField(auto_now_add=True)
+
+	# Model fields
+	# CharField - chars
+	# TextField - massive text
+	# URLField  - for urls
+	# DateTimeField - for times
+
+
+	# 更改顯示方式
+	def __str__(self):
+		return self.title
