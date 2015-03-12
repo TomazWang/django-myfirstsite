3f7511476475f5e69f0702c7543b86dbf2523f5f
Tomaz Wang <TomazWang.tw@gmail.com>
2015-03-12 16:18:14 +0800

    basic hello world

--- trips/views.py
+++ trips/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+# trips/views.py
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+
+
+# simple hello world
+def hello_world(request):
+	return HttpResponse("Hello World")
+
+# use html 
+def hello_html(request):
+	output = """
+		<!DOCTYPE html>
+			<html>
+				<head>
+					<title>Hello Html with Django</title> 
+				</head>
+
+				<body>
+					Hello World!
+					<p>What time is it?</p>
+					<em style="">It's {current_time}</em>
+				</body>
+			</html>
+			""".format(current_time=datetime.now())
+	return HttpResponse(output)
+
+
+# use a template
+def hello_temp(request):
+	return render(request,'hello_temp.html',{'current_time':datetime.now()})
